7a0a8457b860401f42f182ea2b0d31bf5d9eb270
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-12 16:10:52 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Геннадий Ведерников on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
